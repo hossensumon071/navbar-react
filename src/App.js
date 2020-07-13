8cd8d469347774231fee56be87759b658d16e32c
hossensumon071 <hossensumon071@gmail.com>
2020-07-13 19:05:25 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopBanner from "./components/TopBanner/TopBanner";
+import Services from "./components/Services/Services";
+
+
+function App() {
+  return (
+    <div>
+<TopBanner/>
+<Services/>
+
+    </div>
+  );
+}
+
+export default App;
